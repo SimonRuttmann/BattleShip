973de095062108e3d742fb93ac25261336efa1ad
Yannick Soell <yannicksoell@gmail.com>
2020-12-05 01:45:36 +0100

    added Drag&Drop to PlaceShips + isValid indicator: red, green not 100% done

--- src/Controller/PlaceShips.java
+++ src/Controller/PlaceShips.java
@@ -2,18 +2,27 @@ package Controller;
 
 import Gui_View.Main;
 import Player.ActiveGameState;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class PlaceShips implements Initializable {
 
@@ -22,7 +31,13 @@ public class PlaceShips implements Initializable {
     @FXML
     private GridPane ownField;
     @FXML
-    private Button randomPlacement;
+    private Label twoShip;
+    @FXML
+    private Label threeShip;
+    @FXML
+    private Label fourShip;
+    @FXML
+    private Label fiveShip;
     @FXML
     private Label xOfx2Ships;
     @FXML
@@ -32,10 +47,40 @@ public class PlaceShips implements Initializable {
     @FXML
     private Label xOfx5Ships;
     @FXML
+    private Button randomPlacement;
+    @FXML
+    private Button resetPlacement;
+    @FXML
     private Button readyButton;
+
+    String green = "-fx-background-color: #a5fda5";
+    String red = "-fx-background-color: #ffa1a1";
+    String blue = "-fx-background-color: lightblue";
+    String indicateValidPlacement = red;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("window size: " + Main.primaryStage.getHeight());
+        //todo public scale - for fields -> pref size -> better way 2:3 for programm fix -> use SceneSizeChangeListener to scale properly all the time - no strange things happening
+        int scale = 30;
+        // Ships
+        twoShip.setPrefSize(2*scale, scale);
+        twoShip.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/Gui_View/images/2erSchiff.png"))));
+        threeShip.setPrefSize(3*scale, scale);
+        threeShip.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/Gui_View/images/3erSchiff.png"))));
+        fourShip.setPrefSize(4*scale, scale);
+        fourShip.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/Gui_View/images/4erSchiff.png"))));
+        fiveShip.setPrefSize(5*scale, scale);
+        fiveShip.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/Gui_View/images/5erSchiff.png"))));
+
+
+        // Labels
+        xOfx2Ships.setPrefSize(Region.USE_COMPUTED_SIZE, scale);
+        xOfx3Ships.setPrefSize(Region.USE_COMPUTED_SIZE, scale);
+        xOfx4Ships.setPrefSize(Region.USE_COMPUTED_SIZE, scale);
+        xOfx5Ships.setPrefSize(Region.USE_COMPUTED_SIZE, scale);
+
+
         // set Labels to Player Names
         ownFieldLabel.setText(ActiveGameState.getOwnPlayerName() + "'s Spielfeld");
         // 2D field for Labels:
@@ -47,10 +92,75 @@ public class PlaceShips implements Initializable {
         for (int h = 0; h < gamesize; h++) {
             for (int v = 0; v < gamesize; v++) {
                 Label label = new Label();
-                label.setStyle("-fx-background-color: lightblue");
+                label.setStyle(blue);
                 label.setMinSize(5, 5);
                 label.setPrefSize(30, 30);
                 label.setMaxSize(30, 30);
+                // label can recognize Drag
+                int finalH = h;
+                int finalV = v;
+                label.setOnDragOver(event -> {
+                    if (event.getGestureSource() == twoShip) {
+                        System.out.println("two - x: " + finalH + " v: " + finalV);
+                        if(true/*todo valid placement*/) {
+                            event.acceptTransferModes(TransferMode.ANY); //todo - is this needed????
+                            indicateValidPlacement = green;
+                        }
+                        label.setStyle(indicateValidPlacement);
+                        (getNodeByRowColumnIndex(finalV, finalH + 1, ownField)).setStyle(indicateValidPlacement);
+                    }
+                    if (event.getGestureSource() == threeShip) {
+                        System.out.println("three - x: " + finalH + " v: " + finalV);
+                        if(true/*todo valid placement*/) {
+                            event.acceptTransferModes(TransferMode.ANY); //todo - is this needed????
+                            indicateValidPlacement = green;
+                        }
+                        label.setStyle(indicateValidPlacement);
+                        (getNodeByRowColumnIndex(finalV, finalH + 1, ownField)).setStyle(indicateValidPlacement);
+                        (getNodeByRowColumnIndex(finalV, finalH - 1, ownField)).setStyle(indicateValidPlacement);
+                    }
+                    if (event.getGestureSource() == fourShip) {
+                        System.out.println("four - x: " + finalH + " v: " + finalV);
+                        if(true/*todo valid placement*/) {
+                            event.acceptTransferModes(TransferMode.ANY); //todo - is this needed????
+                            indicateValidPlacement = green;
+                        }
+                        label.setStyle(indicateValidPlacement);
+                        (getNodeByRowColumnIndex(finalV, finalH + 1, ownField)).setStyle(indicateValidPlacement);
+                        (getNodeByRowColumnIndex(finalV, finalH - 1, ownField)).setStyle(indicateValidPlacement);
+                        (getNodeByRowColumnIndex(finalV, finalH + 2, ownField)).setStyle(indicateValidPlacement);
+                    }
+                    if (event.getGestureSource() == fiveShip) {
+                        System.out.println("five - x: " + finalH + " v: " + finalV);
+                        if(true/*todo valid placement*/) {
+                            event.acceptTransferModes(TransferMode.ANY); //todo - is this needed????
+                            indicateValidPlacement = green;
+                        }
+                        label.setStyle(indicateValidPlacement);
+                        (getNodeByRowColumnIndex(finalV, finalH + 1, ownField)).setStyle(indicateValidPlacement);
+                        (getNodeByRowColumnIndex(finalV, finalH - 1, ownField)).setStyle(indicateValidPlacement);
+                        (getNodeByRowColumnIndex(finalV, finalH + 2, ownField)).setStyle(indicateValidPlacement);
+                        (getNodeByRowColumnIndex(finalV, finalH - 2, ownField)).setStyle(indicateValidPlacement);
+                    }
+                    event.consume();
+                });
+
+                // remove visual feedback when moving on
+                label.setOnDragExited(event -> {
+                    ObservableList<Node> children = ownField.getChildren();
+                    for (Node labelx: children) {
+                        labelx.setStyle("-fx-background-color: lightblue");
+                    }
+                });
+
+                label.setOnDragDropped(event -> {
+                    // todo make ship placed
+                    System.out.println("Ship placed!");
+                    // is always true - valid placement already called in OnDragOver
+                    event.setDropCompleted(true);
+                    event.consume();
+                });
+
                 GridPane.setConstraints(label, h, v);
                 //todo label in array oder so speichern, um dann darauf zugreifen zu können???
                 ownField.getChildren().addAll(label);
@@ -63,7 +173,61 @@ public class PlaceShips implements Initializable {
         xOfx4Ships.setText("(0/" +ActiveGameState.getAmountShipSize4()+ " platziert)");
         xOfx5Ships.setText("(0/" +ActiveGameState.getAmountShipSize5()+ " platziert)");
 
-        // method for placing ships - thread? drag&drop
+
+        // Drag&Drop Ships -> Placement
+        twoShip.setOnDragDetected(event -> {
+            Dragboard db = twoShip.startDragAndDrop(TransferMode.ANY);
+            ClipboardContent content = new ClipboardContent();
+            content.putString(""); //todo
+            db.setContent(content);
+            db.setDragView(new Image("/Gui_View/images/2erSchiff.png"), 10, 0);
+            event.consume();
+        });
+        threeShip.setOnDragDetected(event -> {
+            Dragboard db = threeShip.startDragAndDrop(TransferMode.ANY);
+            ClipboardContent content = new ClipboardContent();
+            content.putString("");//todo put right content beiing dropped - ship
+            db.setContent(content);
+            db.setDragView(new Image("/Gui_View/images/3erSchiff.png"));
+            event.consume();
+        });
+        fourShip.setOnDragDetected(event -> {
+            Dragboard db = fourShip.startDragAndDrop(TransferMode.ANY);
+            ClipboardContent content = new ClipboardContent();
+            content.putString(""); //todo
+            db.setContent(content);
+            db.setDragView(new Image("/Gui_View/images/4erSchiff.png"), 10 ,0);
+            event.consume();
+        });
+        fiveShip.setOnDragDetected(event -> {
+            Dragboard db = fiveShip.startDragAndDrop(TransferMode.ANY);
+            ClipboardContent content = new ClipboardContent();
+            content.putString(""); //todo
+            db.setContent(content);
+            db.setDragView(new Image("/Gui_View/images/5erSchiff.png"));
+            event.consume();
+        });
+
+        twoShip.setOnDragDone(event -> {
+            System.out.println("Drag done!");
+            // todo disable dropping when all ships placed
+            event.consume();
+        });
+        threeShip.setOnDragDone(event -> {
+            System.out.println("Drag done!");
+            // todo disable dropping when all ships placed
+            event.consume();
+        });
+        fourShip.setOnDragDone(event -> {
+            System.out.println("Drag done!");
+            // todo disable dropping when all ships placed
+            event.consume();
+        });
+        fiveShip.setOnDragDone(event -> {
+            System.out.println("Drag done!");
+            // todo disable dropping when all ships placed
+            event.consume();
+        });
     }
 
     public void newRandomPlacement () {
@@ -97,4 +261,16 @@ public class PlaceShips implements Initializable {
 
     //todo reset label
 
+    public Node getNodeByRowColumnIndex (final int row, final int column, GridPane gridPane) {
+        Node result = null;
+        ObservableList<Node> childrens = gridPane.getChildren();
+
+        for (Node node : childrens) {
+            if(gridPane.getRowIndex(node) == row && gridPane.getColumnIndex(node) == column) {
+                result = node;
+                break;
+            }
+        }
+        return result;
+    }
 }

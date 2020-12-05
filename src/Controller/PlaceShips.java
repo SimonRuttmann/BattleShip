cbd95e864ba4d60c9b84200d76513bc75855c77a
Simon Ruttmann <simon.ruttmann@freenet.de>
2020-12-05 17:13:46 +0100

    Commented PlaceShips.java

--- src/Controller/PlaceShips.java
+++ src/Controller/PlaceShips.java
@@ -62,22 +62,35 @@ public class PlaceShips implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        //Sets the Image for the rotate 90 Degree button
         rotate90.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/Gui_View/images/rotateShipButton.png"))));
+
         System.out.println("window size: " + Main.primaryStage.getHeight());
         //todo public scale - for fields -> pref size -> better way 2:3 for programm fix -> use SceneSizeChangeListener to scale properly all the time - no strange things happening
+
+        //The scale of one Field,   Ship size 2 -> Image: | 30px | 30px |
+        //                          Ship size 3 -> Image: | 30px | 30px | 30px |
+
         int scale = 30;
-        // Ships
+
+
+        // Sets the prefSize depending on the size of the ship
+        // Loads the Image for the Ship's
         twoShip.setPrefSize(2 * scale, scale);
         twoShip.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/Gui_View/images/2erSchiff.png"))));
+
         threeShip.setPrefSize(3 * scale, scale);
         threeShip.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/Gui_View/images/3erSchiff.png"))));
+
         fourShip.setPrefSize(4 * scale, scale);
         fourShip.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/Gui_View/images/4erSchiff.png"))));
+
         fiveShip.setPrefSize(5 * scale, scale);
         fiveShip.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/Gui_View/images/5erSchiff.png"))));
 
 
-        // Labels
+        // Labels e.g. (0 out of 4 Placed)
         xOfx2Ships.setPrefSize(Region.USE_COMPUTED_SIZE, scale);
         xOfx3Ships.setPrefSize(Region.USE_COMPUTED_SIZE, scale);
         xOfx4Ships.setPrefSize(Region.USE_COMPUTED_SIZE, scale);
@@ -91,32 +104,52 @@ public class PlaceShips implements Initializable {
         ownField.setHgap(1);
         ownField.setVgap(1);
 
-        // own Playground
+        // Creates the ownPlayground as a GridPane. Creating a table of Labels, these are later connected to the game playground via the method setLabels()
         for (int h = 0; h < gamesize; h++) {
             for (int v = 0; v < gamesize; v++) {
+
+                //Creating the Labels
                 Label label = new Label();
                 label.setStyle(blue);
                 label.setMinSize(5, 5);
                 label.setPrefSize(30, 30);
                 label.setMaxSize(30, 30);
-                // label can recognize Drag
+
+
+                // finalH is the y position of the label
+                // finalV is the x position of the label
                 int finalH = h;
                 int finalV = v;
+
+                //Every label is getting a OnDragOver-Listener, when a ship is hovered over a label in the playground
+                //The created events set the labels on green when ship placement is valid and red when the ship placement is not
                 label.setOnDragOver(event -> {
-                    // set label color to green or red depending on outcome of valid placement
+
+
+                    //The following is done for every ship label 2, 3, 4, 5
+
+                    // event.getGestureSource() is getting the Element, which is hovered over the item
+                    // In this case the ship Label is returned when hover over an label
                     if (event.getGestureSource() == twoShip) {
+
+
                         System.out.println("two - x: " + finalH + " v: " + finalV);
+                        //When the ship placement is allowed, the String indicateValidPlacement is set to the string green, which contains a -fx css color-scheme
+                        //if the ship placement is not allowed the String is set to the string red, containing another -fx css color-scheme
                         if (true/*todo valid placement - with horizontal*/) {
                             event.acceptTransferModes(TransferMode.ANY); //todo - is this needed????
                             indicateValidPlacement = green;
                         } else
                             indicateValidPlacement = red;
                         label.setStyle(indicateValidPlacement);
+
+                        // Set the style of the labels for all labels, where the ship is hovered over
                         if (horizontal)
                             (getNodeByRowColumnIndex(finalV, finalH + 1, ownField)).setStyle(indicateValidPlacement);
                         else
                             (getNodeByRowColumnIndex(finalV + 1, finalH, ownField)).setStyle(indicateValidPlacement);
                     }
+
                     if (event.getGestureSource() == threeShip) {
                         System.out.println("three - x: " + finalH + " v: " + finalV);
                         if (true/*todo valid placement*/) {
@@ -133,6 +166,7 @@ public class PlaceShips implements Initializable {
                             (getNodeByRowColumnIndex(finalV - 1, finalH, ownField)).setStyle(indicateValidPlacement);
                         }
                     }
+
                     if (event.getGestureSource() == fourShip) {
                         System.out.println("four - x: " + finalH + " v: " + finalV);
                         if (true/*todo valid placement*/) {
@@ -151,6 +185,7 @@ public class PlaceShips implements Initializable {
                             (getNodeByRowColumnIndex(finalV + 2, finalH, ownField)).setStyle(indicateValidPlacement);
                         }
                     }
+
                     if (event.getGestureSource() == fiveShip) {
                         System.out.println("five - x: " + finalH + " v: " + finalV);
                         if (true/*todo valid placement*/) {
@@ -171,8 +206,14 @@ public class PlaceShips implements Initializable {
                             (getNodeByRowColumnIndex(finalV - 2, finalH, ownField)).setStyle(indicateValidPlacement);
                         }
                     }
+
+                    //TODO To Yannick This shouldn`t be necessary? -> Event is consumed when the handle method is executed?
                     event.consume();
                 });
+
+
+                //TODO To Yannick Nicht gut, wenn das Schiff platziert wurde sollen doch nicht ALLE Felder auf lightblue gesetzt werden...
+                //TODO Es sollte eig. garkein Label .setStyle aufrufen, da diese intern gesetzt werden.. Jediglich der "Grüne" Hover muss verschwinden, dafür gibt es bestimmt eine andere methode als setStyle()...
 
                 // remove visual feedback when moving on
                 label.setOnDragExited(event -> {
@@ -182,21 +223,26 @@ public class PlaceShips implements Initializable {
                     }
                 });
 
+
                 label.setOnDragDropped(event -> {
                     // todo make ship placed -> counter erhöhen
                     System.out.println("Ship placed!");
                     // is always true - valid placement already called in OnDragOver
+                    //TODO To Yannick, Gedankenfehler, OnDragOver Event wird immer ausgeführt, egal ob placement valid ist oder nicht (dabei wird ja nur die Labelfarbe verändert)
+                    //TODO To Yannick, Es muss eine Restriction eingebaut werden, bei der ein DragDropped Event nicht ausgeführt werden kann, wenn validPlacement() entsprechenden Rückgabewert gibt
                     event.setDropCompleted(true);
                     event.consume();
                 });
 
                 GridPane.setConstraints(label, h, v);
                 //todo label in array oder so speichern, um dann darauf zugreifen zu können???
+                //TODO To Yannick -> Richtig, einfach aus der anderen Szene Game kopieren um dann setLabels() aufrufen zu können, allerdings ausserhalb der for-schleife
                 ownField.getChildren().addAll(label);
             }
         }
 
-        // set initial Labels - x/x ships placed //todo - 0/ -> variable
+        // Sets the text of the labels e.g (3 out of 7) ships of size 2 placed
+        //todo - 0/ -> variable
         xOfx2Ships.setText("(" + ActiveGameState.getAmountShipSize2placed() + "/" + ActiveGameState.getAmountShipSize2() + " platziert)");
         xOfx3Ships.setText("(" + ActiveGameState.getAmountShipSize3placed() + "/" + ActiveGameState.getAmountShipSize3() + " platziert)");
         xOfx4Ships.setText("(" + ActiveGameState.getAmountShipSize4placed() + "/" + ActiveGameState.getAmountShipSize4() + " platziert)");
@@ -204,6 +250,9 @@ public class PlaceShips implements Initializable {
 
 
         // Drag&Drop Ships -> Placement
+        // For all ship labels with the size 2, 3, 4, 5
+        //TODO To Yannick, für was braucht ein ClipboardContent? Die Funktion die hier ausgeführt werden muss ist doch nur:
+        // Wenn auf ein Schiff geklickt wird und es ist der horizontal wert gesetzt -> Lade Bild 2erSchiff horizontal, sonst Lade Bilde 2er Schiff vertikal
         twoShip.setOnDragDetected(event -> {
             Dragboard db = twoShip.startDragAndDrop(TransferMode.ANY);
             ClipboardContent content = new ClipboardContent();
@@ -215,6 +264,7 @@ public class PlaceShips implements Initializable {
                 db.setDragView(new Image("/Gui_View/images/2erSchiffVertical.png"), 0, -10);
             event.consume();
         });
+
         threeShip.setOnDragDetected(event -> {
             Dragboard db = threeShip.startDragAndDrop(TransferMode.ANY);
             ClipboardContent content = new ClipboardContent();
@@ -226,6 +276,7 @@ public class PlaceShips implements Initializable {
                 db.setDragView(new Image("/Gui_View/images/3erSchiffVertical.png"));
             event.consume();
         });
+
         fourShip.setOnDragDetected(event -> {
             Dragboard db = fourShip.startDragAndDrop(TransferMode.ANY);
             ClipboardContent content = new ClipboardContent();
@@ -237,6 +288,7 @@ public class PlaceShips implements Initializable {
                 db.setDragView(new Image("/Gui_View/images/4erSchiffVertical.png"), 0, -10);
             event.consume();
         });
+
         fiveShip.setOnDragDetected(event -> {
             Dragboard db = fiveShip.startDragAndDrop(TransferMode.ANY);
             ClipboardContent content = new ClipboardContent();
@@ -250,7 +302,9 @@ public class PlaceShips implements Initializable {
             ActiveGameState.setAmountShipSize5placed(ActiveGameState.getAmountShipSize5placed() + 1);
         });
 
-        // disable Dropping when all Ships of each kind are placed
+
+
+        // disable Dropping when all Ships of each kind are placed //TODO To Yannick Falscher Kommentar? <- nicht das Loslassen wird unterbunden, sondern das Klicken auf die Schiffslabels, welche das DragDetected Event auslösen würden
         if (ActiveGameState.getAmountShipSize2() <= ActiveGameState.getAmountShipSize2placed())
             twoShip.setDisable(true);
         if (ActiveGameState.getAmountShipSize3() <= ActiveGameState.getAmountShipSize3placed())
@@ -260,6 +314,7 @@ public class PlaceShips implements Initializable {
         if (ActiveGameState.getAmountShipSize5() <= ActiveGameState.getAmountShipSize5placed())
             fiveShip.setDisable(true);
 
+        //TODO To Yannick wirklich benötigt?
         twoShip.setOnDragDone(event -> {
                 System.out.println("Drag done!");
                 event.consume();

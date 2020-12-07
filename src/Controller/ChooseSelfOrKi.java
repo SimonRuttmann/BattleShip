28bad6f50a6439053909a1aef1fd1c84847fa6d2
Simon Ruttmann <simon.ruttmann@freenet.de>
2020-12-07 20:16:49 +0100

    MultiplayerControlThreadConfigCommunication.java implementation Controls the transmission of the Game settings for the Server and the Client depending on their role
    Right places for the MultiplayerControlThreadConfigCommunication.java must be found, as the GameConfig.java is only called by the host session (Client call is atm at SelfOrKi initialize method)
    
    Thread is atm not working - debugging required
    
    @Yannick
    JavaFx throws error´s due to scene switches in customThreads (must be in JavaFX Application Threads)

--- src/Controller/ChooseSelfOrKi.java
+++ src/Controller/ChooseSelfOrKi.java
@@ -1,21 +1,31 @@
 package Controller;
 
+import Controller.Handler.MultiplayerControlThreadConfigCommunication;
 import Gui_View.Main;
 import Player.ActiveGameState;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ChooseSelfOrKi {
+public class ChooseSelfOrKi implements Initializable {
 
     @FXML public Button playBySelf;
     @FXML public Button letKiPlay;
 
+    //TODO von Simon
+    public void initialize(URL location, ResourceBundle resources) {
+        MultiplayerControlThreadConfigCommunication multiplayerControlThreadConfigCommunication = new MultiplayerControlThreadConfigCommunication();
+        multiplayerControlThreadConfigCommunication.start();
+    }
     public void playSelf() throws IOException {
+
         Parent placeShips = FXMLLoader.load(getClass().getResource("/Gui_View/fxmlFiles/placeShips.fxml"));
         ActiveGameState.setSelfOrKi(true);
         Main.primaryStage.setScene(new Scene(placeShips));
